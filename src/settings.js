89da927fc25a05b6b83243792c2527a8862aa820
Emilia tan <77047823+Sayyiku@users.noreply.github.com>
2022-02-15 00:46:33 +0800

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -2,58 +2,58 @@ const defaultSettings = {
   /**
    * @description 用户未上传头像时使用的默认头像
    */
-  defaultAvatar: 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png',
+  defaultAvatar: 'https://i.w3tt.com/2022/02/11/TftpN.webp',
 
   /**
    * @description 首页背景图
    */
   BannerImg: [
-    {url: 'https://aarabbit.oss-cn-hangzhou.aliyuncs.com/img/%E5%B0%91%E5%A5%B3%E6%88%BF%E9%97%B41920.png'},
-    {url: 'https://aarabbit.oss-cn-hangzhou.aliyuncs.com/img/%E5%8F%AF%E7%88%B1%E7%8C%AB%E8%80%B3%E5%A5%B3%E5%AD%A91980.png'},
-    {url: 'https://aarabbit.oss-cn-hangzhou.aliyuncs.com/img/%E5%B0%91%E5%A5%B3%E5%92%8C%E6%9C%8D1920.png'}
+    {url: 'https://i.w3tt.com/2022/02/12/Tl8dL.jpg'},
+    {url: 'https://i.w3tt.com/2022/02/12/Tl8dL.jpg'},
+    {url: 'https://i.w3tt.com/2022/02/12/Tl8dL.jpg'}
   ],
 
   /**
    * @description 文章详情页背景图
    */
   ArticleImg: [
-    {url: 'https://aarabbit.oss-cn-hangzhou.aliyuncs.com/img/%E5%B0%91%E5%A5%B3%E6%88%BF%E9%97%B41920.png'},
-    {url: 'https://aarabbit.oss-cn-hangzhou.aliyuncs.com/img/%E5%8F%AF%E7%88%B1%E7%8C%AB%E8%80%B3%E5%A5%B3%E5%AD%A91980.png'},
-    {url: 'https://aarabbit.oss-cn-hangzhou.aliyuncs.com/img/%E5%B0%91%E5%A5%B3%E5%92%8C%E6%9C%8D1920.png'}
+    {url: 'https://i.w3tt.com/2022/02/12/Tl8dL.jpg'},
+    {url: 'https://i.w3tt.com/2022/02/12/Tl8dL.jpg'},
+    {url: 'https://i.w3tt.com/2022/02/12/Tl8dL.jpg'}
   ],
 
   /**
    * @description 推荐文章背景图
    */
   Feature: [
-    {img: 'https://aarabbit.oss-cn-hangzhou.aliyuncs.com/img/%E5%90%91%E6%97%A5%E8%91%B5.png'},
-    {img: 'https://aarabbit.oss-cn-hangzhou.aliyuncs.com/img/%E6%89%87%E5%AD%90%E9%81%AE%E8%84%B8.png'},
-    {img: 'https://aarabbit.oss-cn-hangzhou.aliyuncs.com/img/%E6%A1%A5%E8%BE%B9%E5%A5%B3%E5%AD%A9.png'},
-    {img: 'https://aarabbit.oss-cn-hangzhou.aliyuncs.com/img/%E7%A7%8B%E5%A4%A9%E6%9E%AB%E6%A0%91%E7%94%B7%E5%AD%A9%E5%B8%85%E5%93%A5860.png'},
+    {img: 'https://i.w3tt.com/2022/02/12/Tl8dL.jpg'},
+    {img: 'https://i.w3tt.com/2022/02/12/Tl8dL.jpg'},
+    {img: 'https://i.w3tt.com/2022/02/12/Tl8dL.jpg'},
+    {img: 'https://i.w3tt.com/2022/02/12/Tl8dL.jpg'},
   ],
 
   /**
    * @type {String}
    * @description 网页名称
    */
-  title: 'nannan',
+  title: 'Sayyiku',
 
   /**
    * @type {String}
    * @description 头部左侧文字
    */
-  titleLeft: 'nannan',
+  titleLeft: 'Sayyiku',
 
 
   /**
    * @type {String}
    * @description 底部信息设置
    */
-  domain: 'https://nannan.website', //主页链接，点击底部连接时跳转
-  Copyright: 'Copyright @ nannan',
-  footTitle: '南',
+  domain: 'https://sakuravue.vercel.app/', //主页链接，点击底部连接时跳转
+  Copyright: 'Copyright @ Sayyiku',
+  footTitle: 'Sayyiku',
   footTime: '2021-2022',
-  icp: '滇ICP备2021008602号'
+  icp: '萌ICP备20215203号'
 
 }
 
